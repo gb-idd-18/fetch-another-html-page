3fdf4b8d71637df06337dfae2c66784bc5a20d5f
Rocco Panacci <rocco@Roccos-MacBook-Pro.local>
2020-04-02 15:59:52 -0400

    Run some custom JS code when a page loads.

--- js/index.js
+++ js/index.js
@@ -1,9 +1,9 @@
-let loadContentFromHtmlFile = (event) => {
+let loadContentFromHtmlFile = (url) => {
 
 	// Put up the loading screen
 	document.querySelector('.loading').classList.add('show')
 
-	fetch('loadthis.html')
+	fetch(url)
 		.then((response) => {
 			return response.text() // Convert it something readable
 		})
@@ -19,10 +19,36 @@ let loadContentFromHtmlFile = (event) => {
 			// REAPLACE the content, rather than append
 			document.querySelector('#content').innerHTML = newDocument.querySelector('#content').innerHTML
 			
+			// Change the page url by adding "history" to the browser's back/forward buttons
+			window.history.pushState({ /*data can be passed here*/ }, '', `${url}`)
+
+			let $script = document.querySelector('#content').querySelector('script')
+			try {
+				// This has security issues! It's just a demo to simplify the idea
+				eval($script.textContent)
+			} catch(err) {
+				// Shows the console an error, but won't kill the page
+				console.error(`💀 There doesn't appear to be any JavaScript code to run (aaaawkward 😬)`)
+			}
+			
 			// Take down the loading screen, we're done
 			document.querySelector('.loading').classList.remove('show')
 		})
-
 }
 
-document.querySelector('#load').addEventListener('click', loadContentFromHtmlFile)
+
+// MOVED THIS TO INDEX.HTML TO LOCALIZE IT, SINCE IT ONLY APPLIES TO THAT PAGE
+// let testGoingToLoadthis = (event) => {
+// 	loadContentFromHtmlFile('/loadthis.html')
+// }
+// document.querySelector('#load').addEventListener('click', testGoingToLoadthis)
+
+
+// Go load content, otherwise, the url bar changes, but nothing happens in the page
+let browserHistoryWasModified = (event) => {
+	// Go load the page in the url bar
+	loadContentFromHtmlFile(window.location.pathname + window.location.hash)
+}
+
+// If a user goes "back" or "forward" using browser controls but stays on this page
+window.addEventListener('popstate', browserHistoryWasModified)
